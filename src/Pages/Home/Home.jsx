b1fc69cd54b4a0092fec1da81c9017a269c67fc3
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-10-31 23:42:59 +0600

    home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,9 @@
-import React from 'react';
 
 const Home = () => {
-    const click = (e) => {
-        console.log(e.currentTarget)
-    }
+
     return (
-        <section  onClick={click} capture={true}>
-            <div onClick={click} capture={true}>
-                <p onClick={click} capture={true}>click
-                </p>
-            </div>
+        <section>
+            This is home page
         </section>
     );
 };
